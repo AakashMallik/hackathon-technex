2e082551464f74dc00df6507db93da63004de3ad
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-02-09 23:53:05 +0530

    added boiler plate code

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Concept_matching {
+	public String generate_concept(String sentense) {
+		return sentense;
+	}
+}
+
+class Main {
+	public static void main(String[] args) {
+		Concept_matching concept_matching = new Concept_matching();
+		System.out.println(concept_matching.generate_concept("Hello"));
+	}
+}
---
+++ custom_utils/Concept_matcing.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Concept_matching {
+	public String generate_concept(String sentense) {
+		return sentense;
+	}
+}
