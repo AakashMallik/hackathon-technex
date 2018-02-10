2e36ede3c7d25d73996186a6123d86c85873c291
Utsav Krishnan <ketankr9@gmail.com>
2018-02-10 12:47:15 +0530

    minor

--- custom_utils/Matching.java
+++ custom_utils/Matching.java
@@ -1,5 +1,4 @@
-// package custom_utils;
-
+package custom_utils;
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -8,20 +7,20 @@ import java.util.regex.*;
 
 public class Matching {
 
-  static String num_hour = "([0]?[1-9]|1[0-2])"; // 2 digit clock hour
-  static String num_date = "([0]?[1-9]|[1-2][0-9]|3[0-1])"; // date 1 to 31
-  static String num_year = "([1-9]{1}[0-9]{3})"; // Year format
-  static String day = "(today|tomorrow|sun(day)?|mon(day)?|tue(sday)?|wed(nesday)?|thu(rsday)?|fri(day)?|sat(urday)?)";  //any one day, where (sun == sunday), (mon == monday) so on.
-  static String month = "(jan(uary)?|feb(ruary)?|march|april|may|june|july|aug(ust)?|sept(ember)?|oct(ober)?|nov(ember)?|dec(ember)?)";  // any one month, (jan == january) so on.
-  static String date1 = num_date + "(th|rd|st|nd)?\\s+" + month;   // date format 1 (NUMBER MONTH) provided in docx: optional suffix to dates,(eg., 1st, 2nd, 3rd) included.
-  static String date2 = month + "\\s+" + num_date + "(th|rd|st|nd)?"; // date format 2 (MONTH NUMBER) provided in docx.
-  static String date3 = num_date + "(th|rd|st|nd)?\\s+" +month +"\\s+"+num_year; // date format 3 (NUMBER MONTH YEAR) " " "
-  static String date4 = num_year + "\\s+" + month + "\\s+" + num_date + "(th|rd|st|nd)?"; // date format 4 ( YEAR MONTH NUMBER)
-  static String date = "("+date1+"|"+date2+"|"+date3+"|"+date4+")"; // any one of the date formats date1, date2, date3, date4.
+   String num_hour = "([0]?[1-9]|1[0-2])"; // 2 digit clock hour
+   String num_date = "([0]?[1-9]|[1-2][0-9]|3[0-1])"; // date 1 to 31
+   String num_year = "([1-9]{1}[0-9]{3})"; // Year format
+   String day = "(today|tomorrow|sun(day)?|mon(day)?|tue(sday)?|wed(nesday)?|thu(rsday)?|fri(day)?|sat(urday)?)";  //any one day, where (sun == sunday), (mon == monday) so on.
+   String month = "(jan(uary)?|feb(ruary)?|march|april|may|june|july|aug(ust)?|sept(ember)?|oct(ober)?|nov(ember)?|dec(ember)?)";  // any one month, (jan == january) so on.
+   String date1 = num_date + "(th|rd|st|nd)?\\s+" + month;   // date format 1 (NUMBER MONTH) provided in docx: optional suffix to dates,(eg., 1st, 2nd, 3rd) included.
+   String date2 = month + "\\s+" + num_date + "(th|rd|st|nd)?"; // date format 2 (MONTH NUMBER) provided in docx.
+   String date3 = num_date + "(th|rd|st|nd)?\\s+" +month +"\\s+"+num_year; // date format 3 (NUMBER MONTH YEAR) " " "
+   String date4 = num_year + "\\s+" + month + "\\s+" + num_date + "(th|rd|st|nd)?"; // date format 4 ( YEAR MONTH NUMBER)
+   String date = "("+date1+"|"+date2+"|"+date3+"|"+date4+")"; // any one of the date formats date1, date2, date3, date4.
 
 
   // pattern matching function to find if the given pattern contains in a string(sentence).
-  static boolean matchMe(String pattern, String st){
+   private boolean matchMe(String pattern, String st){
     Pattern re = Pattern.compile(pattern,Pattern.CASE_INSENSITIVE);
     Matcher m = re.matcher(st);
     boolean b = m.lookingAt();
@@ -31,43 +30,43 @@ public class Matching {
 
 
   // matching for DateTime Placeholder
-  static boolean isDateTime(String st){
+   public boolean isDateTime(String st){
     String DATE_TIME = "("+day+"|"+date+")?" +"\\s?"+ "("+num_hour+")?" +"\\s?"+ "([ap][\\.]?m)" +"\\s?"+ "("+num_date+"|"+num_hour+")?" +"\\s?" + "("+day+"|"+date+")?";
     return matchMe(DATE_TIME,st);
   }
   // matching for day, today, tomorrow, sunday, mon etc.
-  static boolean isDay(String st){ return matchMe(day,st); }
+   public boolean isDay(String st){ return matchMe(day,st); }
 
   // matching for any of the date format already given, date1,date2,date3,date4
-  static boolean isDate(String st){ return matchMe(date, st); }
+   public boolean isDate(String st){ return matchMe(date, st); }
 
   // matching for <Number> whose range varies from 1-31
-  static boolean isNum(String st){ return matchMe(num_date, st); }
+   public boolean isNum(String st){ return matchMe(num_date, st); }
 
-  static void callme(String st){
+   void callme(String st){
     boolean result = isDay(st);
     System.out.println(st+" -> "+String.valueOf(result));
   }
 
-  static void TestDay(){
+   void TestDay(){
     callme("jan"); callme("fgbgf january fgbfg"); callme("hfdhg 89789 feb dhf"); callme("2018 December 31st");
     callme("Mon"); callme("tuesday");  callme("tomorrow");  callme("friday");
   }
 
-  static void TestMonth() {
+   void TestMonth() {
     callme("jan");
     callme("fgbgf january fgbfg");
     callme("hfdhg 89789 feb dhf");
     callme("2018 December 31st");
   }
 
-  static void TestDate() {
+   void TestDate() {
     callme("5th januray");
     callme("31 oct 2019");
     callme("2018 jan 31st");
   }
 
-  static void Untitled() {
+   void Untitled() {
     callme("am");
     callme("p.m");
     callme("a.m");
@@ -84,8 +83,7 @@ public class Matching {
     callme("32th 2018 february 2 am");
 
   }
-
-  public static void main(String[] args) {
+  public void test() {
     Untitled();
     TestDay();
     TestDate();
