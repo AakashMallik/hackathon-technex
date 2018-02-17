c0fec47be262c60b78a3656ab15530ee2ba86ee1
ketankr9 <ketankr9@gmail.com>
2018-02-18 00:41:25 +0530

    added OpenPhrase.java

---
+++ custom_utils/OpenPhrase.java
@@ -0,0 +1,15 @@
+package custom_utils;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class OpenPhrase {
+
+
+  public void dummy() {
+
+  }
+}
